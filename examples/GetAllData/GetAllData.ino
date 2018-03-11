373345264c170644e801db2f8cbb50f24c74a4d7
Tockn <takuto.sato.5g@stu.hosei.ac.jp>
2018-03-11 22:43:17 +0900

    add gyro angles

--- examples/GetAllData/GetAllData.ino
+++ examples/GetAllData/GetAllData.ino
@@ -4,6 +4,8 @@
 
 MPU6050 mpu6050(Wire);
 
+long timer = 0;
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -13,24 +15,32 @@ void setup() {
 
 void loop() {
   mpu6050.update();
-  Serial.println("=======================================================");
-  Serial.print("temp : ");Serial.println(mpu6050.getTemp());
-  Serial.print("accX : ");Serial.print(mpu6050.getAccX());
-  Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
-  Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
 
-  Serial.print("gyroX : ");Serial.print(mpu6050.getGyroX());
-  Serial.print("\tgyroY : ");Serial.print(mpu6050.getGyroY());
-  Serial.print("\tgyroZ : ");Serial.println(mpu6050.getGyroZ());
-
-  Serial.print("accAngleX : ");Serial.print(mpu6050.getAccAngleX());
-  Serial.print("\taccAngleY : ");Serial.println(mpu6050.getAccAngleY());
+  if(millis() - timer > 1000){
+    
+    Serial.println("=======================================================");
+    Serial.print("temp : ");Serial.println(mpu6050.getTemp());
+    Serial.print("accX : ");Serial.print(mpu6050.getAccX());
+    Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
+    Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
   
-  Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
-  Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
-  Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
-  Serial.println("=======================================================\n");
-
-  delay(1000);
+    Serial.print("gyroX : ");Serial.print(mpu6050.getGyroX());
+    Serial.print("\tgyroY : ");Serial.print(mpu6050.getGyroY());
+    Serial.print("\tgyroZ : ");Serial.println(mpu6050.getGyroZ());
   
+    Serial.print("accAngleX : ");Serial.print(mpu6050.getAccAngleX());
+    Serial.print("\taccAngleY : ");Serial.println(mpu6050.getAccAngleY());
+  
+    Serial.print("gyroAngleX : ");Serial.print(mpu6050.getGyroAngleX());
+    Serial.print("\tgyroAngleY : ");Serial.print(mpu6050.getGyroAngleY());
+    Serial.print("\tgyroAngleZ : ");Serial.println(mpu6050.getGyroAngleZ());
+    
+    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
+    Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
+    Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
+    Serial.println("=======================================================\n");
+    timer = millis();
+    
+  }
+
 }

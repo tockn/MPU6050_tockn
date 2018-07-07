b24256e3f0fc124d13d23663b8fe93208db41a7f
Robert Hutter <rohu747@gmail.com>
2018-07-07 23:45:41 +0100

    Added new methods and fields
    New methods:
    writeBits
    setAccelSensitivity
    setGyroSensitivity

--- src/MPU6050_tockn.cpp
+++ src/MPU6050_tockn.cpp
@@ -1,5 +1,4 @@
 #include "MPU6050_tockn.h"
-#include "Arduino.h"
 
 MPU6050::MPU6050(TwoWire &w){
 	wire = &w;
@@ -19,6 +18,8 @@ void MPU6050::begin(){
 	writeMPU6050(MPU6050_GYRO_CONFIG, 0x08);
 	writeMPU6050(MPU6050_ACCEL_CONFIG, 0x00);
 	writeMPU6050(MPU6050_PWR_MGMT_1, 0x01);
+	setAccelSensitivity(0);
+	setGyroSensitivity(0);
 	this->update();
 	angleGyroX = 0;
 	angleGyroY = 0;
@@ -78,9 +79,9 @@ void MPU6050::calcGyroOffsets(bool console){
 		ry = wire->read() << 8 | wire->read();
 		rz = wire->read() << 8 | wire->read();
 
-		x += ((float)rx) / 65.5;
-		y += ((float)ry) / 65.5;
-		z += ((float)rz) / 65.5;
+		x += ((float)rx) / gyroSensitivity;
+		y += ((float)ry) / gyroSensitivity;
+		z += ((float)rz) / gyroSensitivity;
 	}
 	gyroXoffset = x / 3000;
 	gyroYoffset = y / 3000;
@@ -114,16 +115,16 @@ void MPU6050::update(){
 
 	temp = (rawTemp + 12412.0) / 340.0;
 
-	accX = ((float)rawAccX) / 16384.0;
-	accY = ((float)rawAccY) / 16384.0;
-	accZ = ((float)rawAccZ) / 16384.0;
+	accX = ((float)rawAccX) / accelSensitivity;
+	accY = ((float)rawAccY) / accelSensitivity;
+	accZ = ((float)rawAccZ) / accelSensitivity;
 
 	angleAccX = atan2(accY, accZ + abs(accX)) * 360 / 2.0 / PI;
 	angleAccY = atan2(accX, accZ + abs(accY)) * 360 / -2.0 / PI;
 
-	gyroX = ((float)rawGyroX) / 65.5;
-	gyroY = ((float)rawGyroY) / 65.5;
-	gyroZ = ((float)rawGyroZ) / 65.5;
+	gyroX = ((float)rawGyroX) / gyroSensitivity;
+	gyroY = ((float)rawGyroY) / gyroSensitivity;
+	gyroZ = ((float)rawGyroZ) / gyroSensitivity;
 
 	gyroX -= gyroXoffset;
 	gyroY -= gyroYoffset;
@@ -143,3 +144,93 @@ void MPU6050::update(){
 
 }
 
+/**
+ *	Write bits to a specific register.
+ *
+ * @param regAddress Address of the register to write to
+ * @param startBit First bit position to write
+ * @param length Number of bits to write to
+ * @param data Value to write
+*/
+void MPU6050::writeBits(uint8_t regAddress, uint8_t startBit, uint8_t length, uint8_t data)
+{
+	uint8_t b = readMPU6050(regAddress);
+
+	uint8_t mask = ((1 << length) - 1) << (startBit - length + 1);
+    data <<= (startBit - length + 1); // shift data into correct position
+    data &= mask; // zero all non-important bits in data
+    b &= ~(mask); // zero all important bits in existing byte
+    b |= data; // combine data with existing byte
+		
+	writeMPU6050(regAddress, b);
+}
+
+/** Set full-scale gyro range.
+ * 0 = +/- 250 degrees/sec
+ * 1 = +/- 500 degrees/sec
+ * 2 = +/- 1000 degrees/sec
+ * 3 = +/- 2000 degrees/sec
+ *
+ * @param range New gyro sensitivity setting
+*/
+void MPU6050::setGyroSensitivity(uint8_t range)
+{
+	switch (range)
+	{
+		case 0:
+			gyroSensitivity = MPU6050_250DS_GYRO_SENSITIVITY;
+			break;
+			
+		case 1:
+			gyroSensitivity = MPU6050_500DS_GYRO_SENSITIVITY;
+			break;
+			
+		case 2:
+			gyroSensitivity = MPU6050_1000DS_GYRO_SENSITIVITY;
+			break;
+			
+		case 3:
+			gyroSensitivity = MPU6050_2000DS_GYRO_SENSITIVITY;
+			break;
+			
+		default:
+			return;
+	}
+	
+	writeBits(MPU6050_GYRO_CONFIG, MPU6050_GYRO_CONFIG_FS_SEL_BIT, MPU6050_GYRO_CONFIG_FS_SEL_LENGTH, range);
+}
+
+/** Set full-scale accel range.
+ * 0 = +/- 2g
+ * 1 = +/- 4g
+ * 2 = +/- 8g
+ * 3 = +/- 16g
+ *
+ * @param range New accel sensitivity setting
+ */
+void MPU6050::setAccelSensitivity(uint8_t range)
+{
+	switch (range)
+	{
+		case 0:
+			accelSensitivity = MPU6050_2G_ACCEL_SENSITIVITY;
+			break;
+			
+		case 1:
+			accelSensitivity = MPU6050_4G_ACCEL_SENSITIVITY;
+			break;
+			
+		case 2:
+			accelSensitivity = MPU6050_8G_ACCEL_SENSITIVITY;
+			break;
+			
+		case 3:
+			accelSensitivity = MPU6050_16G_ACCEL_SENSITIVITY;
+			break;
+		
+		default:
+			return;
+	}
+	
+	writeBits(MPU6050_ACCEL_CONFIG, MPU6050_ACCEL_CONFIG_FS_SEL_BIT, MPU6050_ACCEL_CONFIG_FS_SEL_LENGTH, range);
+}
--- src/MPU6050_tockn.h
+++ src/MPU6050_tockn.h
@@ -14,6 +14,22 @@
 #define MPU6050_TEMP_H       0x41
 #define MPU6050_TEMP_L       0x42
 
+#define MPU6050_GYRO_CONFIG_FS_SEL_BIT  4
+#define MPU6050_GYRO_CONFIG_FS_SEL_LENGTH 2
+
+#define MPU6050_ACCEL_CONFIG_FS_SEL_BIT 4
+#define MPU6050_ACCEL_CONFIG_FS_SEL_LENGTH 2
+
+#define MPU6050_2G_ACCEL_SENSITIVITY 16384
+#define MPU6050_4G_ACCEL_SENSITIVITY 8192
+#define MPU6050_8G_ACCEL_SENSITIVITY 4096
+#define MPU6050_16G_ACCEL_SENSITIVITY 2048
+
+#define MPU6050_250DS_GYRO_SENSITIVITY 131
+#define MPU6050_500DS_GYRO_SENSITIVITY 65.5
+#define MPU6050_1000DS_GYRO_SENSITIVITY 32.8
+#define MPU6050_2000DS_GYRO_SENSITIVITY 16.4
+
 class MPU6050{
 	public:
 
@@ -23,6 +39,9 @@ class MPU6050{
 	void begin();
 
 	void setGyroOffsets(float x, float y, float z);
+	
+	void setGyroSensitivity (uint8_t range);
+	void setAccelSensitivity (uint8_t range);
 
 	void writeMPU6050(byte reg, byte data);
 	byte readMPU6050(byte reg);
@@ -74,6 +93,9 @@ class MPU6050{
 	rawGyroX, rawGyroY, rawGyroZ;
 
 	float gyroXoffset, gyroYoffset, gyroZoffset;
+	
+	float accelSensitivity;
+	float gyroSensitivity;
 
 	float temp, accX, accY, accZ, gyroX, gyroY, gyroZ;
 
@@ -82,10 +104,12 @@ class MPU6050{
 
 	float angleX, angleY, angleZ;
 
-  float interval;
+    float interval;
 	long preInterval;
 
 	float accCoef, gyroCoef;
+	
+	void writeBits (uint8_t regAddress, uint8_t startBit, uint8_t length, uint8_t data);
 };
 
 #endif
